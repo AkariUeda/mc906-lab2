a0bfbda87b6e65d8efa9add49ddf31a36f92606c
Tales Lelo <tales.aparecida@gmail.com>
2019-05-17 14:52:42 -0300

    Prepare experiments

--- main.py
+++ main.py
@@ -4,30 +4,33 @@ from time import time
 from evolve import Evolve
 from individual import Individual
 
-def first_experiment():
+def experiment(name, pop_size=20, inner=1, usetime=True, use_interval=False, newgen_parent_ratio=1):
     image = cv2.imread('mona-lisa.jpg')
     image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)  # Convert color loaded by OpenCV
 
     evolve = Evolve(image,
-                    pop_size=50,
+                    pop_size=pop_size,
                     initial_pop=[],
                     ind_size=100,
                     ind_size_max=250,
                     good_genes=True,
                     fitness_function=Individual.get_fitness_function('SSIM_RGB'),
                     crossover_rate=0.9,
-                    use_interval=False, 
-                    newgen_parent_ratio=1.0,
+                    use_interval=use_interval, 
+                    newgen_parent_ratio=newgen_parent_ratio,
                     children_ratio=1,
                     mutation_rate=0.03,
-                    inner_mutation_rate=1,
+                    inner_mutation_rate=inner,
                     unmutable_ratio=0.5,
                     radius_range=(0.01, 0.1))
 
     evolve.evaluate()
-    evolve.save_image('.')
-    print('G:{:4d}\tF:{:.20f}\tT:{:.3f}'.format(evolve.generation, -evolve.solution.fitness, time()))
+    evolve.save_image(name)
+    evolve.save_json(name)
+    print('G:{:4d}\tF:{:.20f}\tT:{:.3f}\tSOL_S:{}\tMAX_S:{}'.format(evolve.generation, -evolve.solution.fitness, time(), evolve.solution.ind_size, max(evolve.pop, key=lambda ind: ind.ind_size).ind_size))
 
+
+    since_enh = 0
     last_best = evolve.solution.fitness
     last_enh = time()
 
@@ -36,19 +39,55 @@ def first_experiment():
         evolve.crossover()
         evolve.mutate(expand_step=1)
         evolve.evaluate()
-        print('G:{:4d}\tF:{:.20f}\tT:{:.3f}'.format(evolve.generation, -evolve.solution.fitness, time()))
+        print('G:{:4d}\tF:{:.20f}\tT:{:.3f}\tSOL_S:{}\tMAX_S:{}'.format(evolve.generation, -evolve.solution.fitness, time(), evolve.solution.ind_size, max(evolve.pop, key=lambda ind: ind.ind_size).ind_size))
         
         if evolve.solution.fitness < last_best:
             last_best = evolve.solution.fitness
             last_enh = time()
-        elif time() - last_enh > 30 * 60:
-            print("No enhancement in the last 30 minutes")
-            break
+        else:
+            if usetime and time() - last_enh > 30 * 60:
+                print("No enhancement in the last 30 minutes")
+                break
+            elif not usetime and since_enh > 30:
+                print("No enhancement in the last 30 generations")
+                break
 
         if i % 10 == 0:
-            evolve.save_image('.')
+            evolve.save_image(name)
+            evolve.save_json(name)
 
+# Popsize
+def experiment_1():
+    experiment('./exp1', pop_size=50)
+
+# Referencia
+def experiment_2():
+    experiment('./exp2')
+
+# Mutação
+def experiment_3():
+    experiment('./exp3', inner=0.1)
+
+# Criterio da parada
+def experiment_4():
+    experiment('./exp4', usetime=False)
+
+# Crossover
+def experiment_5():
+    experiment('./exp5', use_interval=True)
+
+# Seleção
+def experiment_6():
+    experiment('./exp6', newgen_parent_ratio=0)
 
 if __name__ == "__main__":
     # Read reference image
-    first_experiment()
+    # experiment_1()
+    # experiment_2()
+    experiment_3()
+    # experiment_4()
+    # experiment_5()
+    # experiment_6()
+    # experiment_7()
+    # experiment_8()
+    # experiment_9()
